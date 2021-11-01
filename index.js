10b81b4f2fed187fc0fbb7bf6f3751686fe7f18f
alli <>
2021-11-01 19:48:37 -0400

    Updated js

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-// https://api.openweathermap.org/data/2.5/weather?q=tampa&units=imperial&appid=54b4fc35bda7bca5b212ff07c6adce55
-
+// grab references to form, input, and #movie
 var formEl = document.querySelector('form')
 var inputEl = document.querySelector('input')
 var weatherEl = document.getElementById('weather')
-// console.log(formEl, inputEl, weatherEl)
 
+// attach submit to form event
+// fetch movie data if there is a query
+// call render movie function
 formEl.onsubmit = function(e) {
-  e.prevfentDefault()
+  e.preventDefault()
   var weatherQuery = inputEl.value.trim()
-  if (!weatherQuery) return 
-  fetch('https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=54b4fc35bda7bca5b212ff07c6adce55&q=' + weatherQuery)
+  if (!weatherQuery) return
+  fetch('http://api.openweathermap.org/data/2.5/weather?units=imperial&appid=54b4fc35bda7bca5b212ff07c6adce55&q=' + weatherQuery)
   .then(function(res) {
     return res.json()
   })
@@ -18,37 +19,61 @@ formEl.onsubmit = function(e) {
     renderWeather(res)
     inputEl.value = ""
   })
+  .catch(function(err) {
+    console.log(err)
+  })
 }
 
 function renderWeather(weatherObj) {
+  // clear previous movie
   weatherEl.innerHTML = ""
+  // handle movie not found
   if (weatherObj.Response === 'False') {
-    weatherEl.textContent = 'Location Not Found'
+    weatherEl.textContent = 'Location not found'
     return
   }
 
+  // render location
   var locationEl = document.createElement('h2')
-  locationEl.textContent = weatherObj.name + "," + weatherObj.country  //need to replace city/state with real value
+  locationEl.textContent = weatherObj.name + ", " + weatherObj.sys.country
   weatherEl.appendChild(locationEl)
 
-  var mapEl = document.createElement('a')
-  mapEl.textContent = 'Click to view map'
-  mapEl.href = '#' //need to replace href with real value
-  weatherEl.appendChild(mapEl)
+  // render map link
+  // var mapLink = document.createElement('a')
+  // mapLink.textContent = weatherObj.weather[0]
+  // mapLink.href = weatherObj.weather[0]
+  // weatherEl.appendChild(mapLink)
 
+  // render icon image
   var icon = document.createElement('img')
-  icon.src = weatherObj.weather[3] //need to replace icon with real value
+  icon.src = weatherObj.weather.icon
+  icon.alt = weatherObj.weather[0].description
   weatherEl.appendChild(icon)
 
-  var typeOfWeather = document.createElement('h4')
-  typeOfWeather.textContent = weatherObj.weather[2] //need to replace type with real value i.e. sunny, cloudy, etc
-  weatherEl.appendChild(typeOfWeather)
+  // render plot summary
+  var description = document.createElement('h3')
+  description.textContent = weatherObj.weather[0].description
+  weatherEl.appendChild(description)
 
-  var temperatureEl = document.createElement('h4')
-  temperatureEl.textContent = weatherObj.main.temp + weatherObj.main.feels_like //need to replace temperature and feelslike with real value
-  weatherEl.appendChild(temperatureEl)
+    // render temp
+    var temp = document.createElement('h3')
+    temp.textContent = "Current: " +  weatherObj.main.temp
+    weatherEl.appendChild(temp)
 
-  var lastUpdated = document.createElement('h4')
-  lastUpdated.textContent = weatherObj.lastupdated //need to replace lastupdated with real value 
-  weatherEl.appendChild(lastUpdated)
+    // render temp
+    var feelsLike = document.createElement('h3')
+    feelsLike.textContent = "Feels Like: " +  weatherObj.main.feels_like
+    weatherEl.appendChild(feelsLike)
+
+    // render temp
+    var lastUpdated = document.createElement('h3')
+    lastUpdated.textContent = "Last Updated: " + weatherObj.id
+    weatherEl.appendChild(lastUpdated)
+
+  // loop over ratings and render each rating
+  // for (var rating of weatherObj.Ratings) {
+  //   var ratingEl = document.createElement('h5')
+  //   ratingEl.textContent = rating.Source + ': ' + rating.Value
+  //   weatherEl.appendChild(ratingEl)
+  // }
 }
